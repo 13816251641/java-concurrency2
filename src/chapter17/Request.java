0e340b95fbfd27c5a66bfa4c827580a60be91b62
181268 <lujieni1017>
2020-06-04 15:35:49 +0800

    代码提交

--- src/chapter17/Request.java
+++ src/chapter17/Request.java
@@ -5,4 +5,22 @@ package chapter17;
  * @Date 2020/6/2
  */
 public class Request {
+
+    private final String name;
+
+    private final int number;
+
+    public Request(final String name,final int number){
+        this.name=name;
+        this.number=number;
+    }
+
+    public void execute(){
+        System.out.println(Thread.currentThread().getName()+" executed "+this);
+    }
+
+    @Override
+    public String toString() {
+        return "Request=>No. "+number+" Name. "+name;
+    }
 }
--- src/chapter17/TransportThread.java
+++ src/chapter17/TransportThread.java
@@ -4,28 +4,30 @@ import java.util.Random;
 
 /**
  * @Auther lujieni
- * @Date 2020/6/2
+ * @Date 2020/6/4
+ * 类比搬运工,搬到流水线上
  */
-public class WorkerThread extends Thread {
+public class TransportThread extends Thread{
 
     private final Channel channel;
 
     private static final Random random = new Random(System.currentTimeMillis());
 
-    public WorkerThread(String name,Channel channel){
+    public TransportThread(String name,Channel channel){
         super(name);
         this.channel = channel;
     }
 
     @Override
     public void run() {
-        while (true){
-            channel.take().execute();
-            try{
+        try{
+            for(int i=0;true;i++){
+                Request request = new Request(getName(),i);
+                this.channel.put(request);
                 Thread.sleep(random.nextInt(1_000));
-            }catch (InterruptedException e){
-                e.printStackTrace();
             }
+        }catch (Exception e){
+
         }
     }
 }
