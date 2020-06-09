8a09962dd9e0dfbdccc61f5f295eb4b6ef2ab3ef
181268 <lujieni1017>
2020-06-09 13:51:21 +0800

    代码提交

---
+++ src/chapter18/ActiveObject.java
@@ -0,0 +1,14 @@
+package chapter18;
+
+/**
+ * @Auther lujieni
+ * @Date 2020/6/9
+ * 接受异步消息的主动方法
+ */
+public class ActiveObject {
+
+    Result makeString(int count,char fillChar);
+
+    void displayString(String text);
+
+}
---
+++ src/chapter18/MethodRequest.java
@@ -0,0 +1,12 @@
+package chapter18;
+
+/**
+ * @Auther lujieni
+ * @Date 2020/6/9
+ * 对应ActiveObject的每一个方法
+ */
+public abstract class MethodRequest {
+
+    public abstract void execute();
+
+}
